3a06954fb532208643cf165546bbf367fbb84aad
Ken-Yi Feis Lee <feis.tw@gmail.com>
2024-04-28 13:51:35 +0800

    Refactor main.cpp to improve code readability and maintainability

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-template<std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
 class ICell
 {
 public:
@@ -84,7 +84,7 @@ private:
     std::array<std::array<std::shared_ptr<ICell<CELL_SIZE, PADDING_SIZE>>, WIDTH>, HEIGHT> cells_;
 };
 
-template<std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
 class NumberCell : public ICell<CELL_SIZE, PADDING_SIZE>
 {
 public:
@@ -100,11 +100,11 @@ public:
         sf::Color color(r, g, b);
 
         sf::RectangleShape rectangle(
-            sf::Vector2f(CELL_SIZE-2*PADDING_SIZE, CELL_SIZE-2*PADDING_SIZE));
+            sf::Vector2f(CELL_SIZE - 2 * PADDING_SIZE, CELL_SIZE - 2 * PADDING_SIZE));
         rectangle.setOutlineColor(color);
         rectangle.setOutlineThickness(PADDING_SIZE);
         rectangle.setFillColor(sf::Color::Black);
-        rectangle.setPosition(topLeft+ sf::Vector2f(PADDING_SIZE, PADDING_SIZE));
+        rectangle.setPosition(topLeft + sf::Vector2f(PADDING_SIZE, PADDING_SIZE));
         window.draw(rectangle);
 
         sf::Font font;
@@ -119,30 +119,39 @@ public:
         sf::FloatRect rect = text.getLocalBounds();
         text.setOrigin(rect.left + rect.width / 2.0f, rect.top + rect.height / 2.0f);
         text.setPosition(
-            rectangle.getPosition().x + rectangle.getSize().x / 2.0f, 
+            rectangle.getPosition().x + rectangle.getSize().x / 2.0f,
             rectangle.getPosition().y + rectangle.getSize().y / 2.0f);
         window.draw(text);
     }
+
 private:
     int number_;
 };
 
-template<std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
-class CellFactory {
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+class CellFactory
+{
 public:
-    std::shared_ptr<ICell<CELL_SIZE, PADDING_SIZE>> Create() {
+    std::shared_ptr<ICell<CELL_SIZE, PADDING_SIZE>> Create()
+    {
         int val = gen_() % 40;
 
         std::vector<int> numbers = {1, 2, 3, 5, 7, 11};
 
-        if (val == 0) {
+        if (val == 0)
+        {
             return std::make_shared<WallCell<CELL_SIZE, PADDING_SIZE>>();
-        } else if (std::count(numbers.begin(), numbers.end(), val)) {
+        }
+        else if (std::count(numbers.begin(), numbers.end(), val))
+        {
             return std::make_shared<NumberCell<CELL_SIZE, PADDING_SIZE>>(val);
-        } else {
+        }
+        else
+        {
             return std::make_shared<EmptyCell<CELL_SIZE, PADDING_SIZE>>();
         }
     }
+
 private:
     std::mt19937 gen_;
 };
@@ -151,6 +160,7 @@ template <
     std::size_t WIDTH,
     std::size_t HEIGHT,
     std::size_t CELL_SIZE,
+    std::size_t GOAL_SIZE = 4,
     std::size_t PADDING_SIZE = 1>
 class GameManager
 {
@@ -161,20 +171,27 @@ public:
 
         CellFactory<CELL_SIZE, PADDING_SIZE> cellFactory;
 
-        sf::Rect<int> leftGoal(12, 8, 4, 4);
+        sf::Rect<int> leftGoal(
+            WIDTH / 4 - GOAL_SIZE / 2,
+            HEIGHT / 2 - GOAL_SIZE / 2,
+            GOAL_SIZE,
+            GOAL_SIZE);
 
         for (std::size_t i = 0; i < HEIGHT; ++i)
         {
             for (std::size_t j = 0; j < WIDTH / 2; ++j)
             {
                 std::shared_ptr<ICell<CELL_SIZE, PADDING_SIZE>> cell;
-                if (leftGoal.contains(j, i)) {
+                if (leftGoal.contains(j, i))
+                {
                     cell = std::make_shared<GoalCell<CELL_SIZE, PADDING_SIZE>>();
-                } else {
+                }
+                else
+                {
                     cell = cellFactory.Create();
                 }
                 board_.SetCell(i, j, cell);
-                board_.SetCell(HEIGHT-1-i, WIDTH-1-j, cell);
+                board_.SetCell(i, WIDTH - 1 - j, cell);
             }
         }
     }
