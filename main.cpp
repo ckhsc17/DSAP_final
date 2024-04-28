fca62c151b4bc4f98005dff84138bee657300172
Ken-Yi Feis Lee <feis.tw@gmail.com>
2024-04-28 15:54:04 +0800

    Add __memory header to .vscode/settings.json

--- main.cpp
+++ main.cpp
@@ -5,19 +5,12 @@
 #include <SFML/Graphics.hpp>
 
 template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
-class ICell
+class CellRenderer
 {
 public:
-    virtual void Render(sf::RenderWindow &window, sf::Vector2f topLeft) const = 0;
-};
-
-template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
-class EmptyCell : public ICell<CELL_SIZE, PADDING_SIZE>
-{
-public:
-    void Render(sf::RenderWindow &window, sf::Vector2f topLeft) const override
+    static void RenderBorder(sf::RenderWindow &window, sf::Vector2f topLeft)
     {
-        sf::RectangleShape rectangle(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+        sf::RectangleShape rectangle(sf::Vector2f(CELL_SIZE - 2 * PADDING_SIZE, CELL_SIZE - 2 * PADDING_SIZE));
         rectangle.setOutlineColor(sf::Color(60, 60, 60));
         rectangle.setOutlineThickness(PADDING_SIZE);
         rectangle.setFillColor(sf::Color::Black);
@@ -27,10 +20,29 @@ public:
 };
 
 template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
-class WallCell : public ICell<CELL_SIZE, PADDING_SIZE>
+class ICell
 {
 public:
-    void Render(sf::RenderWindow &window, sf::Vector2f topLeft) const override
+    virtual void Render(sf::RenderWindow &window, const sf::Font &font, sf::Vector2f topLeft) const = 0;
+};
+
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+class IForegroundCell : public ICell<CELL_SIZE, PADDING_SIZE>
+{
+public:
+    virtual bool CanModify() const = 0;
+};
+
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+class IBackgroundCell : public ICell<CELL_SIZE, PADDING_SIZE>
+{
+};
+
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+class WallBackgroundCell : public IBackgroundCell<CELL_SIZE, PADDING_SIZE>
+{
+public:
+    void Render(sf::RenderWindow &window, const sf::Font &font, sf::Vector2f topLeft) const override
     {
         sf::RectangleShape rectangle(sf::Vector2f(CELL_SIZE, CELL_SIZE));
         rectangle.setFillColor(sf::Color(60, 60, 60));
@@ -40,75 +52,151 @@ public:
 };
 
 template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
-class GoalCell : public ICell<CELL_SIZE, PADDING_SIZE>
+class CollectionCenterCell : public IForegroundCell<CELL_SIZE, PADDING_SIZE>
 {
 public:
-    void Render(sf::RenderWindow &window, sf::Vector2f topLeft) const override
+    bool CanModify() const override
+    {
+        return false;
+    }
+    void Render(sf::RenderWindow &window, const sf::Font &font, sf::Vector2f topLeft) const override
     {
         sf::RectangleShape rectangle(sf::Vector2f(CELL_SIZE, CELL_SIZE));
-        rectangle.setFillColor(sf::Color(200, 200, 200));
+        rectangle.setFillColor(sf::Color(150, 150, 150));
         rectangle.setPosition(topLeft);
         window.draw(rectangle);
     }
+};
+
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+class CellStack : public ICell<CELL_SIZE, PADDING_SIZE>
+{
+public:
+    std::shared_ptr<IForegroundCell<CELL_SIZE, PADDING_SIZE>> GetForeground() const
+    {
+        return foreground_;
+    }
+    void SetForegrund(std::shared_ptr<IForegroundCell<CELL_SIZE, PADDING_SIZE>> value)
+    {
+        foreground_ = value;
+    }
+    void SetBackground(std::shared_ptr<IBackgroundCell<CELL_SIZE, PADDING_SIZE>> value)
+    {
+        background_ = value;
+    }
+    void Render(sf::RenderWindow &window, const sf::Font &font, sf::Vector2f topLeft) const override
+    {
+        if (foreground_)
+        {
+            foreground_->Render(window, font, topLeft);
+        }
+        else if (background_)
+        {
+            background_->Render(window, font, topLeft);
+        }
+        else
+        {
+            CellRenderer<CELL_SIZE, PADDING_SIZE>::RenderBorder(window, topLeft);
+        }
+    }
+
+private:
+    std::shared_ptr<IForegroundCell<CELL_SIZE, PADDING_SIZE>> foreground_;
+    std::shared_ptr<IBackgroundCell<CELL_SIZE, PADDING_SIZE>> background_;
 };
 
 template <
     std::size_t WIDTH,
     std::size_t HEIGHT,
     std::size_t CELL_SIZE,
-    std::size_t PADDING_SIZE>
+    std::size_t PADDING_SIZE,
+    int TOP = 80,
+    int LEFT = 20>
 class GameBoard
 {
 public:
-    void SetCell(std::size_t i, std::size_t j, std::shared_ptr<ICell<CELL_SIZE, PADDING_SIZE>> value)
-    {
-        cells_[i][j] = value;
+    bool CanModify(sf::Vector2i cellPosition) const {
+        const auto& foregroundCell = cellStacks_[cellPosition.y][cellPosition.x].GetForeground();
+        return foregroundCell == nullptr || foregroundCell->CanModify();
     }
 
-    void Render(sf::RenderWindow &window) const
+    void SetForeground(
+        std::size_t i,
+        std::size_t j,
+        std::shared_ptr<IForegroundCell<CELL_SIZE, PADDING_SIZE>> value)
     {
-        const sf::Vector2f topLeft(20.f, 80.f);
+        cellStacks_[i][j].SetForegrund(value);
+    }
+    void SetBackground(
+        std::size_t i,
+        std::size_t j,
+        std::shared_ptr<IBackgroundCell<CELL_SIZE, PADDING_SIZE>> value)
+    {
+        cellStacks_[i][j].SetBackground(value);
+    }
 
+    sf::Vector2i GetMouseCellPosition(const sf::RenderWindow &window) const
+    {
+        const sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+        const sf::Vector2i relatedMousePosition = mousePosition - sf::Vector2i(LEFT, TOP);
+        return sf::Vector2i(relatedMousePosition.x / CELL_SIZE, relatedMousePosition.y / CELL_SIZE);
+    }
+
+    bool IsMouseInsideBoard(const sf::RenderWindow &window) const
+    {
+        sf::Vector2i mousePosition = GetMouseCellPosition(window);
+        return mousePosition.x >= 0 && mousePosition.x < WIDTH &&
+               mousePosition.y >= 0 && mousePosition.y < HEIGHT;
+    }
+
+    void Render(sf::RenderWindow &window, const sf::Font &font) const
+    {
         for (std::size_t i = 0; i < HEIGHT; ++i)
         {
             for (std::size_t j = 0; j < WIDTH; ++j)
             {
-                cells_[i][j]->Render(
+                cellStacks_[i][j].Render(
                     window,
-                    topLeft + sf::Vector2f(j * CELL_SIZE, i * CELL_SIZE));
+                    font,
+                    sf::Vector2f(LEFT + j * CELL_SIZE, TOP + i * CELL_SIZE));
             }
         }
+
+        if (!IsMouseInsideBoard(window))
+        {
+            return;
+        }
+
+        sf::Vector2i mousePosition = GetMouseCellPosition(window);
+
+        sf::Text currentCellPositionText;
+        currentCellPositionText.setString("(" + std::to_string(mousePosition.x) + ", " + std::to_string(mousePosition.y) + ")");
+        currentCellPositionText.setFont(font);
+        currentCellPositionText.setCharacterSize(20);
+        currentCellPositionText.setPosition(20, 20);
+        window.draw(currentCellPositionText);
     }
 
 private:
-    std::array<std::array<std::shared_ptr<ICell<CELL_SIZE, PADDING_SIZE>>, WIDTH>, HEIGHT> cells_;
+    std::array<std::array<CellStack<CELL_SIZE, PADDING_SIZE>, WIDTH>, HEIGHT> cellStacks_;
 };
 
 template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
-class NumberCell : public ICell<CELL_SIZE, PADDING_SIZE>
+class NumberBackgroundCell : public IBackgroundCell<CELL_SIZE, PADDING_SIZE>
 {
 public:
-    NumberCell(int number) : number_(number) {}
+    NumberBackgroundCell(int number) : number_(number) {}
 
-    void Render(sf::RenderWindow &window, sf::Vector2f topLeft) const override
+    void Render(sf::RenderWindow &window, const sf::Font &font, sf::Vector2f topLeft) const override
     {
+        CellRenderer<CELL_SIZE, PADDING_SIZE>::RenderBorder(window, topLeft);
+
         std::mt19937 gen(number_);
         std::uniform_int_distribution<int> dis(0, 50);
         int r = dis(gen) + 128;
         int g = dis(gen) + 128;
         int b = dis(gen) + 128;
         sf::Color color(r, g, b);
-
-        sf::RectangleShape rectangle(
-            sf::Vector2f(CELL_SIZE - 2 * PADDING_SIZE, CELL_SIZE - 2 * PADDING_SIZE));
-        rectangle.setOutlineColor(color);
-        rectangle.setOutlineThickness(PADDING_SIZE);
-        rectangle.setFillColor(sf::Color::Black);
-        rectangle.setPosition(topLeft + sf::Vector2f(PADDING_SIZE, PADDING_SIZE));
-        window.draw(rectangle);
-
-        sf::Font font;
-        font.loadFromFile("/Library/Fonts/Arial Unicode.ttf");
 
         sf::Text text;
         text.setFont(font);
@@ -119,8 +207,8 @@ public:
         sf::FloatRect rect = text.getLocalBounds();
         text.setOrigin(rect.left + rect.width / 2.0f, rect.top + rect.height / 2.0f);
         text.setPosition(
-            rectangle.getPosition().x + rectangle.getSize().x / 2.0f,
-            rectangle.getPosition().y + rectangle.getSize().y / 2.0f);
+            topLeft.x + CELL_SIZE / 2.0f,
+            topLeft.y + CELL_SIZE / 2.0f);
         window.draw(text);
     }
 
@@ -129,10 +217,10 @@ private:
 };
 
 template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
-class CellFactory
+class BackgroundCellFactory
 {
 public:
-    std::shared_ptr<ICell<CELL_SIZE, PADDING_SIZE>> Create()
+    std::shared_ptr<IBackgroundCell<CELL_SIZE, PADDING_SIZE>> Create()
     {
         int val = gen_() % 40;
 
@@ -140,20 +228,81 @@ public:
 
         if (val == 0)
         {
-            return std::make_shared<WallCell<CELL_SIZE, PADDING_SIZE>>();
+            return std::make_shared<WallBackgroundCell<CELL_SIZE, PADDING_SIZE>>();
         }
         else if (std::count(numbers.begin(), numbers.end(), val))
         {
-            return std::make_shared<NumberCell<CELL_SIZE, PADDING_SIZE>>(val);
+            return std::make_shared<NumberBackgroundCell<CELL_SIZE, PADDING_SIZE>>(val);
         }
         else
         {
-            return std::make_shared<EmptyCell<CELL_SIZE, PADDING_SIZE>>();
+            return nullptr;
         }
     }
 
 private:
     std::mt19937 gen_;
+};
+
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+class LeftToRightConveyorCell : public IForegroundCell<CELL_SIZE, PADDING_SIZE>
+{
+public:
+    bool CanModify() const override
+    {
+        return true;
+    }
+    void Render(sf::RenderWindow &window, const sf::Font &font, sf::Vector2f topLeft) const override
+    {
+        CellRenderer<CELL_SIZE, PADDING_SIZE>::RenderBorder(window, topLeft);
+        int yOffset = 2;
+        sf::RectangleShape rectangle(sf::Vector2f(CELL_SIZE, CELL_SIZE - 2 * yOffset));
+        rectangle.setFillColor(sf::Color(128, 128, 128));
+        rectangle.setPosition(topLeft + sf::Vector2f(0, yOffset));
+        window.draw(rectangle);
+    }
+};
+
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+class BottomToUpConveyorCell : public IForegroundCell<CELL_SIZE, PADDING_SIZE>
+{
+public:
+    bool CanModify() const override
+    {
+        return true;
+    }
+    void Render(sf::RenderWindow &window, const sf::Font &font, sf::Vector2f topLeft) const override
+    {
+        CellRenderer<CELL_SIZE, PADDING_SIZE>::RenderBorder(window, topLeft);
+
+        int xOffset = 2;
+        sf::RectangleShape rectangle(sf::Vector2f(CELL_SIZE - 2 * xOffset, CELL_SIZE));
+        rectangle.setFillColor(sf::Color(128, 128, 128));
+        rectangle.setPosition(topLeft + sf::Vector2f(xOffset, 0));
+        window.draw(rectangle);
+    }
+};
+
+template <std::size_t CELL_SIZE, std::size_t PADDING_SIZE>
+class BottomToRightConveyorCell : public IForegroundCell<CELL_SIZE, PADDING_SIZE>
+{
+public:
+    void Render(sf::RenderWindow &window, const sf::Font &font, sf::Vector2f topLeft) const override
+    {
+        CellRenderer<CELL_SIZE, PADDING_SIZE>::RenderBorder(window, topLeft);
+
+        int xOffset = 2;
+        sf::ConvexShape shape(6);
+        shape.setPoint(0, sf::Vector2f(xOffset, CELL_SIZE));
+        shape.setPoint(1, sf::Vector2f(xOffset, 4 * xOffset));
+        shape.setPoint(2, sf::Vector2f(4 * xOffset, xOffset));
+        shape.setPoint(3, sf::Vector2f(CELL_SIZE, xOffset));
+        shape.setPoint(4, sf::Vector2f(CELL_SIZE, CELL_SIZE - xOffset));
+        shape.setPoint(5, sf::Vector2f(CELL_SIZE - xOffset, CELL_SIZE));
+        shape.setFillColor(sf::Color(128, 128, 128));
+        shape.setPosition(topLeft);
+        window.draw(shape);
+    }
 };
 
 template <
@@ -169,30 +318,56 @@ public:
     {
         static_assert(WIDTH % 2 == 0, "WIDTH must be even");
 
-        CellFactory<CELL_SIZE, PADDING_SIZE> cellFactory;
+        BackgroundCellFactory<CELL_SIZE, PADDING_SIZE> backgroundCellFactory;
 
-        sf::Rect<int> leftGoal(
+        // Setup Background
+        for (std::size_t i = 0; i < HEIGHT; ++i)
+        {
+            for (std::size_t j = 0; j < WIDTH / 2; ++j)
+            {
+                std::shared_ptr<IBackgroundCell<CELL_SIZE, PADDING_SIZE>> backgroundCell =
+                    backgroundCellFactory.Create();
+                board_.SetBackground(i, j, backgroundCell);
+                board_.SetBackground(i, WIDTH - 1 - j, backgroundCell);
+            }
+        }
+
+        sf::Rect<int> leftCollectionCenter(
             WIDTH / 4 - GOAL_SIZE / 2,
             HEIGHT / 2 - GOAL_SIZE / 2,
             GOAL_SIZE,
             GOAL_SIZE);
 
-        for (std::size_t i = 0; i < HEIGHT; ++i)
+        auto leftCollectionCenterCell = std::make_shared<CollectionCenterCell<CELL_SIZE, PADDING_SIZE>>();
+
+        for (int i = leftCollectionCenter.top; i < leftCollectionCenter.top + leftCollectionCenter.height; ++i)
         {
-            for (std::size_t j = 0; j < WIDTH / 2; ++j)
+            for (int j = leftCollectionCenter.left; j < leftCollectionCenter.left + leftCollectionCenter.width; ++j)
             {
-                std::shared_ptr<ICell<CELL_SIZE, PADDING_SIZE>> cell;
-                if (leftGoal.contains(j, i))
-                {
-                    cell = std::make_shared<GoalCell<CELL_SIZE, PADDING_SIZE>>();
-                }
-                else
-                {
-                    cell = cellFactory.Create();
-                }
-                board_.SetCell(i, j, cell);
-                board_.SetCell(i, WIDTH - 1 - j, cell);
+                board_.SetForeground(i, j, leftCollectionCenterCell);
             }
+        }
+    }
+
+    bool IsMouseInsideBoard(const sf::RenderWindow &window) {
+        return board_.IsMouseInsideBoard(window);
+    }
+
+    void Build(const sf::RenderWindow &window)
+    {
+        sf::Vector2i mouseCellPosition = board_.GetMouseCellPosition(window);
+        if (board_.CanModify(mouseCellPosition)) {
+            board_.SetForeground(mouseCellPosition.y, mouseCellPosition.x, std::make_shared<LeftToRightConveyorCell<CELL_SIZE, PADDING_SIZE>>());
+        }
+    }
+
+    void Clear(const sf::RenderWindow& window) 
+    {
+        sf::Vector2i mouseCellPosition = board_.GetMouseCellPosition(window);
+        
+        if (board_.CanModify(mouseCellPosition))
+        {
+            board_.SetForeground(mouseCellPosition.y, mouseCellPosition.x, nullptr);
         }
     }
 
@@ -200,9 +375,10 @@ public:
     {
     }
 
-    void Render(sf::RenderWindow &window) const
+    void Render(sf::RenderWindow &window, const sf::Font &font) const
     {
-        board_.Render(window);
+        window.clear(sf::Color::Black);
+        board_.Render(window, font);
     }
 
 private:
@@ -211,6 +387,9 @@ private:
 
 int main(int, char **)
 {
+    sf::Font font;
+    font.loadFromFile("/Library/Fonts/Arial Unicode.ttf");
+
     sf::VideoMode mode = sf::VideoMode(1280, 1024);
 
     sf::RenderWindow window(mode, "DSAP Final Project");
@@ -222,12 +401,28 @@ int main(int, char **)
         sf::Event event;
         while (window.pollEvent(event))
         {
+            if (event.type == sf::Event::MouseButtonReleased)
+            {
+                if (gameManager.IsMouseInsideBoard(window)) 
+                {
+                    if (event.mouseButton.button == sf::Mouse::Left) 
+                    {
+                        gameManager.Build(window);
+                    } 
+                    else 
+                    {
+                        gameManager.Clear(window);
+                    }
+                }
+            }
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
         }
 
         gameManager.Update();
-        gameManager.Render(window);
+        gameManager.Render(window, font);
         window.display();
     }
 }
