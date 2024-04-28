013148b1e1f01b1d15247c7ddfec7f945ecdd116
Ken-Yi Feis Lee <feis.tw@gmail.com>
2024-04-28 12:27:56 +0800

    Add .vscode/settings.json file and update main.cpp with game board and game manager classes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,66 @@
 #include <iostream>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
-int main(int, char**){
-    sf::VideoMode mode = sf::VideoMode(1024, 1024/4*3);
-    
-    sf::Window window(mode, "DSAP Final Project");
+template <
+    std::size_t WIDTH,
+    std::size_t HEIGHT,
+    std::size_t CELL_SIZE,
+    std::size_t PADDING_SIZE = 1>
+class GameBoard
+{
+public:
+    void Render(sf::RenderWindow &window) const
+    {
+        const sf::Vector2f topLeft(20.f, 20.f);
+
+        sf::RectangleShape rectangle(
+            sf::Vector2f(
+                CELL_SIZE - 2 * PADDING_SIZE,
+                CELL_SIZE - 2 * PADDING_SIZE));
+
+        rectangle.setFillColor(sf::Color(128, 128, 128));
+
+        for (std::size_t i = 0; i < HEIGHT; ++i)
+        {
+            for (std::size_t j = 0; j < WIDTH; ++j)
+            {
+                rectangle.setPosition(
+                    topLeft.x + j * CELL_SIZE + PADDING_SIZE,
+                    topLeft.y + i * CELL_SIZE + PADDING_SIZE);
+                window.draw(rectangle);
+            }
+        }
+    }
+
+private:
+    std::array<std::array<bool, WIDTH>, HEIGHT> cells_;
+};
+
+template <std::size_t WIDTH, std::size_t HEIGHT, std::size_t CELL_SIZE>
+class GameManager
+{
+public:
+    void Update()
+    {
+    }
+
+    void Render(sf::RenderWindow &window) const
+    {
+        board_.Render(window);
+    }
+
+private:
+    GameBoard<WIDTH, HEIGHT, CELL_SIZE> board_;
+};
+
+int main(int, char **)
+{
+    sf::VideoMode mode = sf::VideoMode(1280, 1024);
+
+    sf::RenderWindow window(mode, "DSAP Final Project");
+
+    GameManager<50, 40, 20> gameManager;
 
     while (window.isOpen())
     {
@@ -14,5 +70,9 @@ int main(int, char**){
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        gameManager.Update();
+        gameManager.Render(window);
+        window.display();
     }
 }
